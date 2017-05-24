2f46903059a5ad64b9286da01e3761ef81ff8dd7
k.zmorzynski <k.zmorzynski@gmail.com>
2017-05-24 17:40:17 +0200

    added simple helper tool to easier input @R(..) and @S(..) in cshtml files

---
+++ src/Jockey/MainForm.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Jockey
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if(disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this._text = new ScintillaNET.Scintilla();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openRootDirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.undoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this._autoSaveFiles = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this._files = new System.Windows.Forms.TreeView();
+            this.menuStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // _text
+            // 
+            this._text.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._text.Lexer = ScintillaNET.Lexer.Html;
+            this._text.Location = new System.Drawing.Point(0, 0);
+            this._text.Name = "_text";
+            this._text.Size = new System.Drawing.Size(587, 495);
+            this._text.TabIndex = 0;
+            this._text.KeyDown += new System.Windows.Forms.KeyEventHandler(this._text_KeyDown);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.undoToolStripMenuItem,
+            this.sToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(790, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.openRootDirToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(145, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // openRootDirToolStripMenuItem
+            // 
+            this.openRootDirToolStripMenuItem.Name = "openRootDirToolStripMenuItem";
+            this.openRootDirToolStripMenuItem.Size = new System.Drawing.Size(145, 22);
+            this.openRootDirToolStripMenuItem.Text = "Open root dir";
+            this.openRootDirToolStripMenuItem.Click += new System.EventHandler(this.openRootDirToolStripMenuItem_Click);
+            // 
+            // undoToolStripMenuItem
+            // 
+            this.undoToolStripMenuItem.Name = "undoToolStripMenuItem";
+            this.undoToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.undoToolStripMenuItem.Text = "Undo";
+            this.undoToolStripMenuItem.Click += new System.EventHandler(this.undoToolStripMenuItem_Click);
+            // 
+            // sToolStripMenuItem
+            // 
+            this.sToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this._autoSaveFiles});
+            this.sToolStripMenuItem.Name = "sToolStripMenuItem";
+            this.sToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.sToolStripMenuItem.Text = "Options";
+            // 
+            // _autoSaveFiles
+            // 
+            this._autoSaveFiles.CheckOnClick = true;
+            this._autoSaveFiles.Name = "_autoSaveFiles";
+            this._autoSaveFiles.Size = new System.Drawing.Size(147, 22);
+            this._autoSaveFiles.Text = "Autosave files";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(790, 28);
+            this.toolStrip1.TabIndex = 5;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(557, 25);
+            this.toolStripLabel1.Text = "F5 = @S(\"...\")     F6 = @R(\"...\")     F7 = R(\"...\")       F8 = next file";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 52);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this._files);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this._text);
+            this.splitContainer1.Size = new System.Drawing.Size(790, 495);
+            this.splitContainer1.SplitterDistance = 199;
+            this.splitContainer1.TabIndex = 6;
+            // 
+            // _files
+            // 
+            this._files.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._files.HideSelection = false;
+            this._files.Location = new System.Drawing.Point(0, 0);
+            this._files.Name = "_files";
+            this._files.Size = new System.Drawing.Size(199, 495);
+            this._files.TabIndex = 0;
+            this._files.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this._files_AfterSelect);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(790, 547);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "MainForm";
+            this.Text = "Main form";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ScintillaNET.Scintilla _text;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openRootDirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem undoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem _autoSaveFiles;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TreeView _files;
+    }
+}
+
---
+++ src/Jockey/MainForm.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Jockey
+{
+    public partial class MainForm : Form
+    {
+        private string _rootFolder;
+        private string _appBaseDir;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void setStyle( ScintillaNET.Scintilla scintilla )
+        {
+            // Reset the styles
+            scintilla.StyleResetDefault();
+            scintilla.Styles[ScintillaNET.Style.Default].Font = "Consolas";
+            scintilla.Styles[ScintillaNET.Style.Default].Size = 10;
+            scintilla.StyleClearAll();
+            
+            // Show line numbers
+            scintilla.Margins[0].Width = 20;
+
+            // Enable folding
+            scintilla.SetProperty("fold", "1");
+            scintilla.SetProperty("fold.compact", "1");
+            scintilla.SetProperty("fold.html", "1");
+
+            // Use Margin 2 for fold markers
+            scintilla.Margins[2].Type = ScintillaNET.MarginType.Symbol;
+            scintilla.Margins[2].Mask = ScintillaNET.Marker.MaskFolders;
+            scintilla.Margins[2].Sensitive = true;
+            scintilla.Margins[2].Width = 20;
+
+            // Reset folder markers
+            for(int i = ScintillaNET.Marker.FolderEnd; i <= ScintillaNET.Marker.FolderOpen; i++)
+            {
+                scintilla.Markers[i].SetForeColor(SystemColors.ControlLightLight);
+                scintilla.Markers[i].SetBackColor(SystemColors.ControlDark);
+            }
+
+            // Style the folder markers
+            scintilla.Markers[ScintillaNET.Marker.Folder].Symbol = ScintillaNET.MarkerSymbol.BoxPlus;
+            scintilla.Markers[ScintillaNET.Marker.Folder].SetBackColor(SystemColors.ControlText);
+            scintilla.Markers[ScintillaNET.Marker.FolderOpen].Symbol = ScintillaNET.MarkerSymbol.BoxMinus;
+            scintilla.Markers[ScintillaNET.Marker.FolderEnd].Symbol = ScintillaNET.MarkerSymbol.BoxPlusConnected;
+            scintilla.Markers[ScintillaNET.Marker.FolderEnd].SetBackColor(SystemColors.ControlText);
+            scintilla.Markers[ScintillaNET.Marker.FolderMidTail].Symbol = ScintillaNET.MarkerSymbol.TCorner;
+            scintilla.Markers[ScintillaNET.Marker.FolderOpenMid].Symbol = ScintillaNET.MarkerSymbol.BoxMinusConnected;
+            scintilla.Markers[ScintillaNET.Marker.FolderSub].Symbol = ScintillaNET.MarkerSymbol.VLine;
+            scintilla.Markers[ScintillaNET.Marker.FolderTail].Symbol = ScintillaNET.MarkerSymbol.LCorner;
+
+            // Enable automatic folding
+            scintilla.AutomaticFold = ScintillaNET.AutomaticFold.Show | ScintillaNET.AutomaticFold.Click | ScintillaNET.AutomaticFold.Change;
+
+            // Set the Styles
+            scintilla.StyleResetDefault();
+            // I like fixed font for XML
+            scintilla.Styles[ScintillaNET.Style.Default].Font = "Courier";
+            scintilla.Styles[ScintillaNET.Style.Default].Size = 10;
+            scintilla.StyleClearAll();
+            scintilla.Styles[ScintillaNET.Style.Xml.Attribute].ForeColor = Color.Red;
+            scintilla.Styles[ScintillaNET.Style.Xml.Entity].ForeColor = Color.Red;
+            scintilla.Styles[ScintillaNET.Style.Xml.Comment].ForeColor = Color.Green;
+            scintilla.Styles[ScintillaNET.Style.Xml.Tag].ForeColor = Color.Blue;
+            scintilla.Styles[ScintillaNET.Style.Xml.TagEnd].ForeColor = Color.Blue;
+            scintilla.Styles[ScintillaNET.Style.Xml.DoubleString].ForeColor = Color.DeepPink;
+            scintilla.Styles[ScintillaNET.Style.Xml.SingleString].ForeColor = Color.DeepPink;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            setStyle(_text);
+            _text.Lexer = ScintillaNET.Lexer.Html;
+            _appBaseDir = AppDomain.CurrentDomain.BaseDirectory;
+            string sf = _appBaseDir + "lastfolder.txt";
+            if(File.Exists(sf))
+                _rootFolder = File.ReadAllText(sf);
+        }
+
+        private void _text_KeyDown(object sender, KeyEventArgs evt)
+        {
+            if(evt.KeyCode == Keys.F5 || evt.KeyCode == Keys.F6 || evt.KeyCode == Keys.F7)
+            {
+                // check vincity of selection
+                var selections = _text.Selections;
+                if(selections == null || selections.Count == 0)
+                    return;
+                var selection = selections[0];
+                int s = selection.Start;
+                int e = selection.End;
+                var curText = _text.SelectedText;
+                if(s > 0)
+                {
+                    // check if we have " before
+                    var ch = _text.GetCharAt(s - 1);
+                    if(ch == '"') //if preceding char is " then expand selection
+                        s--;
+                }
+                while(e < _text.TextLength - 1)
+                {
+                    var ch = _text.GetCharAt(e);
+                    if(ch == '.' || ch == ':') //if next char is special then expand selection
+                    {
+                        e++;
+                        curText = curText + (char)ch;
+                    }
+                    else if(ch == '"')
+                        e++;
+                    else
+                        break;
+                }
+                selection.Start = s;
+                selection.End = e;
+                string symbol = (evt.KeyCode == Keys.F5 ? "S" : "R");
+
+                string newText = (evt.KeyCode == Keys.F5 || evt.KeyCode == Keys.F6 ? "@" : "") + symbol + "(\"" + curText + "\")";
+                _text.DeleteRange(s, e - s);
+                _text.InsertText(s, newText);
+                
+                evt.Handled = true;
+            }
+            else if(evt.KeyCode == Keys.F8)
+            {
+                if(_files.SelectedNode != null)
+                {
+                    if(_files.SelectedNode.Tag != null && _autoSaveFiles.Checked)
+                    {
+                        var fp = _files.SelectedNode.Tag as string;
+                        if(File.Exists(fp))
+                        {
+                            File.WriteAllText(fp, _text.Text, Encoding.UTF8);
+                        }
+                    }
+                    var nextNode = _files.SelectedNode.NextVisibleNode;
+                    while(nextNode != null)
+                    {
+                        if(nextNode.Tag != null)
+                        {
+                            _files.SelectedNode = nextNode;
+                            break;
+                        }
+                        nextNode = nextNode.NextVisibleNode;
+                    }
+                }
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(_files.SelectedNode != null && _files.SelectedNode != null)
+            {
+                var fp = _files.SelectedNode.Tag as string;
+                if(File.Exists(fp))
+                {
+                    File.WriteAllText(fp, _text.Text, Encoding.UTF8);
+                }
+            }
+        }
+
+        private void openRootDirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dlg = new FolderBrowserDialog();
+            if(_rootFolder != null)
+                dlg.SelectedPath = _rootFolder;
+            if(dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                _rootFolder = dlg.SelectedPath;
+                // clear treeview
+                _files.Nodes.Clear();
+                // go over all dirs and get cshtml files
+                var allFiles = Directory.EnumerateFiles(_rootFolder, "*.cshtml", SearchOption.AllDirectories);
+                foreach(var filePath in allFiles)
+                {
+                    var relativeFilePath = filePath.Substring(_rootFolder.Length + 1);
+                    addNode(filePath, relativeFilePath);
+                }
+                _files.ExpandAll();
+                File.WriteAllText(_appBaseDir + "lastfolder.txt", _rootFolder);
+            }
+        }
+
+        private void addNode(string filePath, string relativeFilePath)
+        {
+            // split relative file path by separator
+            var elems = relativeFilePath.Split(Path.DirectorySeparatorChar);
+            // check if treeview has node already
+            TreeNodeCollection curNodes = _files.Nodes;
+            for(int i = 0; i < elems.Length; ++i)
+            {
+                var nodeName = elems[i];
+                var nodes = curNodes.Find(nodeName, false);
+                if(nodes != null && nodes.Length == 1)
+                {
+                    curNodes = nodes[0].Nodes;
+                }
+                else
+                {
+                    // create new node
+                    var newNode = curNodes.Add(nodeName, nodeName);
+                    if(i == elems.Length - 1)
+                        newNode.Tag = filePath; // we are at the end
+                    curNodes = newNode.Nodes;
+                }
+            }
+        }
+
+
+        private void undoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _text.Undo();
+        }
+
+        private void _files_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            if(e.Node == null)
+                return;
+
+            var fp = e.Node.Tag as string;
+            if(File.Exists(fp))
+            {
+                _text.Text = File.ReadAllText(fp, Encoding.UTF8);
+            }
+        }
+    }
+}
